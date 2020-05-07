59db899086c13dfdb6058e9decd2256bbb6559d1
John Gerula <jgerula@objectsharp.com>
2020-05-07 11:31:05 -0400

    - added SignalR client(s) and hub, successfully reproduces issue

---
+++ Server/ChatHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            user = Context?.User?.Identity?.Name ?? user;
+            await Clients.All.SendAsync($"ReceiveMessage", user, message);
+        }
+    }
+}
--- Server/Startup.cs
+++ Server/Startup.cs
@@ -1,13 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Server.IISIntegration;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 namespace Server
 {
@@ -23,33 +18,34 @@ namespace Server
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(o =>
+                o.AddPolicy(
+                    "Cors",
+                    b => b.
+                        WithOrigins("https://localhost:44353").
+                        AllowAnyHeader().
+                        AllowAnyMethod().
+                        AllowCredentials().
+                        SetIsOriginAllowed(h => true)));
             services.AddRazorPages();
+            services.AddSignalR();
+            services.AddAuthentication(IISDefaults.AuthenticationScheme);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
 
+            app.UseCors("Cors");
+            app.UseAuthentication();
+
             app.UseRouting();
-
             app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
+                endpoints.MapHub<ChatHub>("/chathub");
             });
         }
     }
